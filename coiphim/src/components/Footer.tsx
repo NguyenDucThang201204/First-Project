6ff7051377426f34946d610a2655dbfd237432d2
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-12-27 23:32:29 +0700

    Add footer,header,movie components

---
+++ coiphim/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">© 2025 Coi Phim. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
